e4129cb7d2b0a7e9c9850166c953081bd7a491af
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-11-11 14:38:58 -0500

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,37 @@
+public class Triangle {
+  private int lines;
+
+  public Triangle(int lines) {
+    this.lines = lines;
+    display();
+  }
+
+  public void display() {
+    int spaces = lines - 1;
+    int last;
+    int min = 1;
+    int max;
+
+    for (int i = 0; i < lines; i++) {
+      last = 2 * i + 1;
+      max = spaces + last;
+
+      for (int j = min; j <= max; j++) {
+        if (j == spaces + 1 || j == max || i == lines - 1)
+          System.out.print('*');
+        else
+          System.out.print(' ');
+      }
+
+      System.out.println();
+      spaces--;
+    }
+  }
+
+  public static void main(String[] args) {
+    new Triangle(3);
+    new Triangle(4);
+    new Triangle(5);
+    new Triangle(6);
+  }
+}
